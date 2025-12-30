b1931f9756d3dd73dc7f40e977b124c7d8902833
joel <joelseb@gmail,com>
2025-12-30 08:26:59 +0530

    insertion sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void swap(int arr[],int j){
+  int temp = arr[j];
+  arr[j] = arr[j-1];
+  arr[j-1] = temp;
+}
+
+void insertion_sort(int arr[],int n){
+  for(int i=1;i<=n-1;i++){
+    int j =i;
+    while(j>0 && arr[j-1]> arr[j]){
+      swap(arr,j);
+      j--;
+    }
+  }
+}
+
+int main(){
+  int n;
+  cin>> n;
+  int arr[n];
+  for(int i = 0;i<n;i++) cin>> arr[i];
+  insertion_sort(arr,n);
+  for(int i=0;i<n;i++) cout<< arr[i]<< " ";
+
+  return 0;
+}
